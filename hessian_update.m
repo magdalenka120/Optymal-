05737d4f178ca946356e5c6f3e6ebce37e872989
magdalenka120 <magdalenka120@gmail.com>
2015-11-02 11:59:30 +0100

    Create hessian_update.m

---
+++ hessian_update.m
@@ -0,0 +1,31 @@
+NIEUZUPENIONE
+
+
+# Optymal-%--------------------------------------------------------------------------
+%-----------------------  Optymalizacja systemów  -------------------------
+%--------------------------------------------------------------------------
+% Zadanie 1: Detekcja twarzy
+% autorzy: A. Gonczarek, J.M. Tomczak
+% 2014
+%--------------------------------------------------------------------------
+
+function BinvNew = hessian_update(Binv,gradDiff,step)
+%Funkcja wylicza rekurencyjną poprawkę do odwróconego hesjanu. 
+% Binv - bieżąca wartość odwróconego hesjanu
+% gradDiff - różnia gradientów
+% step - wykonany krok optymalizacji
+
+BinvNew = zeros(size(Binv));
+
+%--------------------------------------------------------------------------
+%--------------------- TUTAJ WLASNA IMPLEMENTACJA -------------------------
+%--------------------------------------------------------------------------
+
+
+
+
+
+
+
+%--------------------------------------------------------------------------
+end
